da123d73fb4ccf4f706a2ea73e96d2f0632d21bd
PipGrylls <pipgryllsastro@gmail.com>
2023-11-29 15:26:47 +0000

    typo fix

--- src/date.cc
+++ src/date.cc
@@ -44,7 +44,7 @@ void Subs::Date::add_day(int nday){
 int Subs::Date::day() const{
     int d,m,y,status;
     double fd;
-	status = iauJd2cal(DMJ0, double(mjd_)+0.1,&y, &m, &d, &fd);
+	status = iauJd2cal(MJD0, double(mjd_)+0.1,&y, &m, &d, &fd);
     return d;
 }
  
@@ -53,7 +53,7 @@ int Subs::Date::day() const{
 int Subs::Date::month() const{
     int d,m,y,status;
     double fd;
-    status = iauJd2cal(DMJ0, double(mjd_)+0.1, &y, &m, &d, &fd);
+    status = iauJd2cal(MJD0, double(mjd_)+0.1, &y, &m, &d, &fd);
     return m;
 }
 
@@ -62,7 +62,7 @@ int Subs::Date::month() const{
 int Subs::Date::year() const{
     int d,m,y,status;
     double fd;
-    status = iauJd2cal(DMJ0, double(mjd_)+0.1, &y, &m, &d, &fd);
+    status = iauJd2cal(MJD0, double(mjd_)+0.1, &y, &m, &d, &fd);
     return y;
 }
 
@@ -76,7 +76,7 @@ int Subs::Date::year() const{
 void Subs::Date::date(int& day_, int& month_, int& year_) const {
     int status;
     double fd;
-	status = iauJd2cal(DMJ0, double(mjd_)+0.1,&year_, &month_, &day_, &fd);
+	status = iauJd2cal(MJD0, double(mjd_)+0.1,&year_, &month_, &day_, &fd);
 }
 
 /** Returns modified Julian day number,
